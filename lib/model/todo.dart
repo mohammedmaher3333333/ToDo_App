8375923f51a51555e00dfca803b76f66efae949b
mohammedmaher3333333 <87941592+mohammedmaher3333333@users.noreply.github.com>
2024-07-16 00:21:00 +0200

    first up

---
+++ lib/model/todo.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/cupertino.dart';
+//
+// class ToDo {
+//   final String taskName;
+//   final bool taskCompleted;
+//   Function(bool?)? onChanged;
+//   Function(BuildContext)? deleteFunction;
+//
+//   ToDo({
+//     required this.taskName,
+//     required this.taskCompleted,
+//     this.onChanged,
+//     this.deleteFunction,
+//   });
+// }
